c6f24fe6a315305d90502979ca357a6868953928
Lee Danilek <lee.danilek@yale.edu>
2017-05-01 23:16:48 -0400

    expressions to flow tables

--- Expression.java
+++ Expression.java
@@ -1,8 +1,28 @@
-import java.util.Map;
+import java.util.*;
 
 // evaluates to an integer
 abstract class Expression {
+    public ExpressionResult asFlowTables(FlowTable jumpTo) {
+        return null;
+    }
 
+    private static int dummyVarCounter = 0;
+    public String newDummyVar() {
+        String dummy = "dummy"+dummyVarCounter;
+        dummyVarCounter++;
+        return dummy;
+    }
+}
+
+class ExpressionResult {
+    // to compute the thing
+    public ArrayList<FlowTable> tables;
+    // where is the thing now?
+    public MatchableField field;
+    public ExpressionResult(ArrayList<FlowTable> tables, MatchableField field) {
+        this.tables = tables;
+        this.field = field;
+    }
 }
 
 class Variable extends Expression {
@@ -10,12 +30,22 @@ class Variable extends Expression {
     public Variable(String var) {
         variable = var;
     }
+
+    @Override
+    public ExpressionResult asFlowTables(FlowTable jumpTo) {
+        return new ExpressionResult(new ArrayList<FlowTable>(), new MatchableField(variable));
+    }
 }
 
 class PacketValue extends Expression {
     public PacketField field;
     public PacketValue(PacketField field) {
         this.field = field;
+    }
+
+    @Override
+    public ExpressionResult asFlowTables(FlowTable jumpTo) {
+        return new ExpressionResult(new ArrayList<FlowTable>(), new MatchableField(field));
     }
 }
 
@@ -31,11 +61,40 @@ class LookUp extends Expression {
         this.map = map;
         this.exp = exp;
     }
+
+    @Override
+    public ExpressionResult asFlowTables(FlowTable jumpTo) {
+        FlowTable table = new FlowTable(new Header(new MatchableField[0]), new Row[0]);
+        ExpressionResult subResult = this.exp.asFlowTables(table);
+        MatchableField[] matching = {subResult.field};
+        table.header = new Header(matching);
+        String output = this.newDummyVar();
+        ArrayList<Row> rows = new ArrayList<Row>();
+        for (Map.Entry<Integer, Integer> e : this.map.entrySet()) {
+            Cell[] cells = {new Cell(e.getKey().intValue(), ~0, subResult.field)};
+            Action[] actions = {new AssignVariableAction(output, e.getValue().intValue())};
+            rows.add(new Row(1, actions, cells));
+        }
+        table.rows = (Row[])(rows.toArray());
+        ArrayList<FlowTable> tables = new ArrayList<FlowTable>();
+        tables.add(table);
+        return new ExpressionResult(tables, new MatchableField(output));
+    }
 }
 
 class Constant extends Expression {
     public int val;
     public Constant(int val) {
         this.val = val;
+    }
+    @Override
+    public ExpressionResult asFlowTables(FlowTable jumpTo) {
+        String output = this.newDummyVar();
+        Action[] actions = {new AssignVariableAction(output, val)};
+        Row[] rows = {new Row(1, actions, new Cell[0])};
+        FlowTable table = new FlowTable(new Header(new MatchableField[0]), rows);
+        ArrayList<FlowTable> tables = new ArrayList<FlowTable>();
+        tables.add(table);
+        return new ExpressionResult(tables, new MatchableField(output));
     }
 }
--- Statement.java
+++ Statement.java
@@ -2,8 +2,37 @@ import java.util.*;
 
 abstract class Statement {
     // TODO: make methods to return inputs/outputs for dataflow
-    public ArrayList<FlowTable> asFlowTables() {
-        return new FlowTable[0];
+    public ArrayList<FlowTable> asFlowTables(FlowTable jumpTo) {
+        return new ArrayList<FlowTable>();
+    }
+
+    // helper functions for subclasses
+    public FlowTable flowTableForActions(Action[] actions) {
+        MatchableField dummyField = new MatchableField(PacketField.InPort);
+        MatchableField[] dummyFields = {dummyField};
+        Header header = new Header(dummyFields);
+        Cell[] matchAll = {new Cell(0, 0, dummyField)};
+        Row row = new Row(1, actions, matchAll);
+        Row[] rows = {row};
+        return new FlowTable(header, rows);
+    }
+
+    public Action[] actionsFollowedByJump(Action[] actions, FlowTable jumpTo) {
+        if (jumpTo != null) {
+            int length = actions.length;
+            actions = new Action[length+1];
+            actions[length] = new JumpAction(jumpTo.index);
+        }
+        return actions;
+    }
+
+    public ArrayList<FlowTable> flowTablesForAction(Action action, FlowTable jumpTo) {
+        Action[] actions = {action};
+        actions = this.actionsFollowedByJump(actions, jumpTo);
+        FlowTable table = this.flowTableForActions(actions);
+        ArrayList<FlowTable> tables = new ArrayList<FlowTable>();
+        tables.add(table);
+        return tables;
     }
 }
 
@@ -45,22 +74,40 @@ class AssignField extends Statement {
         this.field = field;
         value = val;
     }
-}
 
-class ValidateCheckSum extends Statement {}
-
-class RecomputeCheckSum extends Statement {}
-
-class Drop extends Statement {
-    public ArrayList<FlowTable> asFlowTables() {
-        FlowTable table = new FlowTable();
-        ArrayList<FlowTable> tables = new ArrayList<FlowTable>();
-        tables.add(table);
-        return tables;
+    @Override
+    public ArrayList<FlowTable> asFlowTables(FlowTable jumpTo) {
+        return this.flowTablesForAction(new RecomputeChecksumAction(), jumpTo);
     }
 }
 
-class Forward extends Statement {}
+class VerifyChecksum extends Statement {
+    @Override
+    public ArrayList<FlowTable> asFlowTables(FlowTable jumpTo) {
+        return this.flowTablesForAction(new VerifyChecksumAction(), jumpTo);
+    }
+}
+
+class RecomputeCheckSum extends Statement {
+    @Override
+    public ArrayList<FlowTable> asFlowTables(FlowTable jumpTo) {
+        return this.flowTablesForAction(new RecomputeChecksumAction(), jumpTo);
+    }
+}
+
+class Drop extends Statement {
+    @Override
+    public ArrayList<FlowTable> asFlowTables(FlowTable jumpTo) {
+        return this.flowTablesForAction(new DropAction(), jumpTo);
+    }
+}
+
+class Forward extends Statement {
+    @Override
+    public ArrayList<FlowTable> asFlowTables(FlowTable jumpTo) {
+        return this.flowTablesForAction(new ForwardAction(), jumpTo);
+    }
+}
 
 class Sequence extends Statement {
     public Statement stm1;
