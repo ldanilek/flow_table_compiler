48cffbe247376a0fb153d1fd5bc4c6a9be97b187
Tyler Petrochko <ty.petrochko@gmail.com>
2017-05-01 21:45:09 -0400

    Remodeled flowtable structure

--- FlowTable.java
+++ FlowTable.java
@@ -1,22 +1,61 @@
+/* Written by Tyler/Lee
+ *
+ * Note - may need to change some of these classes to public to make them
+ * accessible outside this file!
+ */
 
-class Match {
+/* The header row for a flow table, specifying column vals */
+class Header {
+    public MatchableField[] fields;
+
+    public Header(MatchableField[] fields){
+        this.fields = fields;
+    }
+
+    public String printable(){
+        String str = "Header Fields:\t";
+        for(int i = 0; i < fields.length; i++){
+           str += fields[i].printable();
+        }
+
+        return str;
+    }
+}
+
+/* Contained within a row, the individual cells of a flowtable */
+class Cell{
     public int bitVector;
     public int mask;
-    public int priority;
-    public Action[] actions;
+    public MatchableField field;
 
-    public Match(int bitVector, int mask, int priority, Action[] actions) {
+    public Cell (int bitVector, int mask, MatchableField field){
+        this.field = field;
         this.bitVector = bitVector;
         this.mask = mask;
-        // TODO: what to match on? variable? packet attribute?
+    }
+}
+
+/* An individual row of a flowtable - fields of cells must match those in
+ * header! Any fields in the header that don't have corresponding cells should
+ * just default to wildcard (i.e. don't match on that field) */
+class Row {
+    public int priority;
+    public Action[] actions;
+    public Cell[] cells;
+
+    public Row(int priority, Action[] actions, Cell[] cells) {
         this.priority = priority;
         this.actions = actions;
+        this.cells = cells;
     }
 
     public String printable() {
-        String str = Integer.toBinaryString(bitVector) + "\t"
-            + Integer.toBinaryString(mask)
-            + "\t" + Integer.toString(priority) + "\t{";
+        String str = Integer.toString(priority) + "\t";
+        for(int i = 0; i < cells.length; i++){
+           str += Integer.toBinaryString(cells[i].bitVector) + "\t"
+            + Integer.toBinaryString(cells[i].mask) + "\t";
+        }
+        str += "{";
         for (int i = 0; i < actions.length; i++) {
             str += actions[i].printable() + "; ";
         }
@@ -27,18 +66,21 @@ class Match {
 class FlowTable {
     private static int nextIndex = 0;
     public int index;
-    public Match[] matches;
+    public Header header;
+    public Row[] rows;
 
-    public FlowTable(int index, Match[] matches) {
+    public FlowTable(Header header, Row[] rows) {
         this.index = nextIndex;
         nextIndex++;
-        this.matches = matches;
+        this.header = header;
+        this.rows = rows;
     }
 
     public String printable() {
         String str = "Flow Table "+Integer.toString(index)+"\n";
-        for (int i = 0; i < matches.length; i++) {
-            str += matches[i].printable() + "\n";
+        str += header.printable() + "\n";
+        for (int i = 0; i < rows.length; i++) {
+            str += rows[i].printable() + "\n";
         }
         return str;
     }
---
+++ Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Statement tree =
+        //     new IF(
+        //         new Compare(new PacketValue(PacketField.InPort),
+        //             CompareOperation.LT, 80),
+        //         new Drop(),
+        //         new Sequence(
+        //             new AssignField(PacketField.TTL,
+        //                 new Decrement(new PacketValue(PacketField.TTL))),
+        //             new Forward()));
+        // Statement simpleTree = new Drop();
+        // Action[] actions = {new DropAction()};
+        // Match[] matches = {new Match(0, 0, 1, actions)};
+        // FlowTable table = new FlowTable(0, matches);
+        // System.out.println(table.printable());
+    }
+}
